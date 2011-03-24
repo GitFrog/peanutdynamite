2509fa9c89a256b854b574881870dd09ed2c385d
Lee Hulse-Smith <lee.hulsesmith@gmail.com>
2011-03-23 22:23:51 -0400

    database update

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DeadGrandmaCookies::Application.config.secret_token = '11f6177741a912c4edfb9053ebbfe51acb9fd75e0ebf6d7b5f1c587dbc251ce3dc4bf5ac2c8fb66c67777d0ea626b70f3b4c3e945043991f99a6f3929f932ec8'
