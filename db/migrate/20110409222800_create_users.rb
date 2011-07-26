42241e9127a1f1f806ffb23916d574e594bb5d85
Lee Hulse-Smith <lee.hulsesmith@gmail.com>
2011-07-26 10:44:38 -0400

    troubleshoot 12

--- db/migrate/20110409222800_create_users.rb
+++ db/migrate/20110409222800_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :email, :limit => 50
       t.string :name, :limit => 50
       t.string :bookname, :limit => 50
+      t.string :email, :limit => 50
       t.string :encrypted_password
       t.string :salt
       t.timestamps
