cb92f92a5209981b642c84aa371191d227e04b11
Lee Hulse-Smith <lee.hulsesmith@gmail.com>
2011-07-30 23:08:56 -0400

    keeper and maybe logo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def facebook_like
+    content_tag :iframe, nil, :src => "http://www.facebook.com/plugins/like.php?href=#{CGI::escape(request.url)}&layout=standard&show_faces=true&width=450&action=like&font=arial&colorscheme=light&height=80", :scrolling => 'no', :frameborder => '0', :allowtransparency => true, :id => :facebook_like
+  end
 end
